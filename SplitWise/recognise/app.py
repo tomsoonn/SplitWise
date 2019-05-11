a85322e5c38aa690f5f1c39876955600acf20b4f
Klaudia-PC\Klaudia <klaudia.budzynska@quantoo.pl>
2019-05-11 22:23:31 +0200

    Scanning receipt & recognising full amount from receipt

---
+++ SplitWise/recognise/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from receipt_reader import find_amount
+
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('img_string', type=str)
+
+class ReceiptReader(Resource):
+    def post(self):
+        args = parser.parse_args()
+        img_string = str(args['img_string'])
+        res = find_amount(img_string)
+        return {'amount': res}
+
+    def get(self):
+        return "Wyślij w body POST pod img_string string zawierający obraz przekonwertowany na base64 np {'img_string' : 'base64'}"
+
+api.add_resource(ReceiptReader, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ SplitWise/recognise/receipt_reader.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import re
+
+def send_request(content):
+    URL = 'https://vision.googleapis.com/v1/images:annotate?key='
+
+    content = {'content': content}
+    features = [{'type': 'DOCUMENT_TEXT_DETECTION'}]
+    img_context = {'languageHints': ['pl']}
+    data = {'requests': [
+        {'image': content, 'features': features, 'imageContext': img_context}]}
+    req = requests.post(URL, data=json.dumps(data))
+    return req.text
+
+def find_amount(img_string):
+    response_text = send_request(img_string)
+    response = json.loads(response_text)["responses"][0]
+    if "textAnnotations" in response:
+        text = response["textAnnotations"][0]["description"]
+        suma = re.search(".*SU[MHN]A PLN[ \n]([0-9]+[,.]?[0-9]*).*", text)
+        if suma is not None:
+            return suma.group(1)
+    return 0
